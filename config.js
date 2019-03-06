28ec70bf2e7a9135af9a76bcebd637547c68dcdf
Spacenow <39359888+spacenowteam@users.noreply.github.com>
2019-03-07 08:40:19 +1100

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-export const accessKeyId      = process.env.ACCESS_KEY_ID || 'AKIAJVDE2MQXGXCZFQLQ';
-export const secretAccessKey  = process.env.SECRET_ACCESS_KEY || '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';
-export const Bucket           = process.env.BUCKET || 'spacenow-listing-photos';
-export const BucketPath       = process.env.BUCKET_PATH || 'uploads';
